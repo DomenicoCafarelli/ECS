747c716eff9208675777da42b6b1ee0971cca235
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-11 15:58:13 +0100

    Project created

---
+++ SocialNetworkECS/SocialNetworkECS/SocialNetworkECSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkECSApp.swift
+//  SocialNetworkECS
+//
+//  Created by Domenico Cafarelli on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkECSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
