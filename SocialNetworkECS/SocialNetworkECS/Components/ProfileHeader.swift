6415093b64f1f8a627c6104a560a7ddb248b6409
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-12 16:23:32 +0100

    Added the Swift Lint Created the Design Pattern Path (MVVM)
    Created the component header

---
+++ SocialNetworkECS/SocialNetworkECS/Components/ProfileHeader.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileHeader.swift
+//  SocialNetworkECS
+//
+//  Created by Domenico Cafarelli on 11/01/23.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ProfileHeader: View {
+    
+    @State private var selectedItem: PhotosPickerItem? = nil
+    @State private var showingOptions = false
+    @State private var selection = "None"
+    
+    var body: some View {
+        
+        VStack(spacing: 22){
+            
+//            PhotosPicker(
+//                selection: $selectedItem,
+//                matching: .images,
+//                photoLibrary: .shared()) {
+//                    Text("Select a photo")
+//                }
+            
+            Button{
+                showingOptions = true
+            }label: {
+                
+                Image("prova")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 90, height: 90)
+                    .clipShape(Circle())
+                
+            }.confirmationDialog("Choose an action", isPresented: $showingOptions, titleVisibility: .visible) {
+                Button("View Photo") {
+                    selection = "View Photo"
+                }
+                
+                Button("Edit Photo") {
+                    selection = "Edit Photo"
+                    print("edit")
+                }
+                
+                Button("Delete Photo", role: .destructive) {
+                    selection = "Delete Photo"
+                        
+                }
+            }
+            
+            
+            
+            
+            Text("Domenico Cafarelli")
+            
+        }.font(.title3).bold()
+    }
+}
+
+struct ProfileHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHeader()
+    }
+}
---
+++ SocialNetworkECS/SocialNetworkECS/View/AddPostView.swift
@@ -0,0 +1,20 @@
+//
+//  AddPostView.swift
+//  SocialNetworkECS
+//
+//  Created by Domenico Cafarelli on 11/01/23.
+//
+
+import SwiftUI
+
+struct AddPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPostView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPostView()
+    }
+}
