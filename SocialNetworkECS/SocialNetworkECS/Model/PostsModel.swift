618fa61751dbe1d166cfff2747543686bbfb6af9
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-12 16:30:36 +0100

    created the posts model and ViewModel files

---
+++ SocialNetworkECS/SocialNetworkECS/Model/PostsModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostsModel.swift
+//  SocialNetworkECS
+//
+//  Created by Domenico Cafarelli on 12/01/23.
+//
+
+import Foundation
---
+++ SocialNetworkECS/SocialNetworkECS/ViewModel/PostsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostsViewModel.swift
+//  SocialNetworkECS
+//
+//  Created by Domenico Cafarelli on 12/01/23.
+//
+
+import Foundation
